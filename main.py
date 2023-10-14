ffc3d50c80f98f411165f06f1a4e2bc440bec108
EvgrafovMichail <autumnevilmen@gmail.com>
2023-10-14 14:52:22 +0300

    ts-1-tester-problem: change structure

--- main.py
+++
@@ -1,7 +0,0 @@
-# from test_system.testers.tester import Tester
-# from testcases_hw2 import testcases
-
-
-# if __name__ == '__main__':
-#     tester = Tester()
-#     tester.get_report('submissions/', testcases)
