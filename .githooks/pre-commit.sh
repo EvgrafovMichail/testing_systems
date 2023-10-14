dcfe9a2eef795349e004c34b2af9cf41fca49330
EvgrafovMichail <autumnevilmen@gmail.com>
2023-10-14 17:54:48 +0300

    update hooks

--- .githooks/pre-commit.sh
+++ .githooks/pre-commit.sh
@@ -1,7 +1,7 @@
 echo "[INFO]: start pre-commit script"
 echo "[INFO]: flake8 stage"
 
-python -m flake8 src/test_system
+python -m flake8
 
 if [ $? -ne 0 ]; then
     echo "[ERROR]: flake8 stage failed"
